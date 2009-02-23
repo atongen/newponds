d95730827202bc2088decfbbded9f29a7f2e02f7
Andrew Tongen <andrew@avalanche.coop>
2009-02-23 16:28:40 -0600

    added some test stuff

---
+++ index2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+    <head>
+        <title>New Ponds</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="style/style2.css" type="text/css" media="screen">
+    </head>
+    <body>
+        <table id="container">
+            <tr>
+                <td id="header" colspan="2">
+                    <h1>New Ponds</h1>
+                    <ul id="toptabs" class="tabs">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Torkel's Hjem</a></li>
+                        <li><a href="">Eagle's Watch</a></li>
+                    </ul>
+                    <div id="header-image">img</div>
+                </td>
+            </tr>
+            <tr>
+                <td id="sidebar">
+                    <p>sidebar</p>
+                </td>
+                <td id="content">
+                    <p>content</p>
+                </td>
+            </tr>
+            <tr>
+                <td id="footer" colspan="2">
+                    <?php include 'includes/footer.php' ?>
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
