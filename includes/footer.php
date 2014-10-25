eaaff937ec4c3f774e629486f85b978b5500c0d9
Andrew Tongen <atongen@gmail.com>
2014-10-25 13:38:27 -0500

    Remove php

--- includes/footer.php
+++
@@ -1,3 +0,0 @@
-<div style="line-height: 50px; text-align: center;">
-    &copy; Copyright <?php echo date('Y'); ?> New Ponds Media
-</div>
--- includes/menu.php
+++
@@ -1,8 +0,0 @@
-<?php function this_script($script) {
-    return end(explode('/', $_SERVER['SCRIPT_NAME'])) == $script;
-} ?>
-<ul id="nav">
-    <li class="link<?php if(this_script('index.php')) echo ' current_link'; ?>"><a href="index.php">Home</a></li>
-    <li class="link<?php if(this_script('torkels_hjem.php')) echo ' current_link'; ?>"><a href="torkels_hjem.php">Torkel's Hjem</a></li>
-    <li class="link<?php if(this_script('eagles_watch.php')) echo ' current_link'; ?>"><a href="eagles_watch.php">Eagle's Watch</a></li>
-</ul>
